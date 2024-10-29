76f39ebf0848ad84083372b0bef311742193e920
Jesus <jesus.calrod.1@educa.jcyl.es>
2024-10-29 14:50:49 +0100

    Interfaz echa

--- app/src/main/java/com/example/practica1pmdm/MainActivity.kt
+++ app/src/main/java/com/example/practica1pmdm/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.practica1pmdm
 
 import android.os.Bundle
+import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        var suscripcion = findViewById<Switch>(R.id.switchBoletin)
+
 
     }
 }
